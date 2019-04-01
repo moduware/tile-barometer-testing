9619aa4a520dba9f15828f21803b3c0fcba37473
Alex Chernov <alexusblack@gmail.com>
2019-04-02 10:51:50 +1100

    adding density altitude and altimeter to output

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
+var PSL = 1013.25; // Pressure at sea level hPa
+var TSL = 288.15; // Temperature at sea level Kelvin
+var TLR = 0.0065; // Temperature Lapse rate K/m
+var IGC = 8.3144598; // Ideal gas constant J/(mol K)
+var g = 9.80665; // gravitational acceleration m/s2
+var MDA = 0.028964; // molar mass of dry air kg/mol
+var MW = 0.018; // molar mass of water vapor
+
 var lastTimeRecieved = 0;
-var lastData = {
-	humidity: null,
-	temperature: null,
-	pressure: null
-};
+// var lastData = {
+// 	humidity: null,
+// 	temperature: null,
+// 	pressure: null
+// };
 
 /* =========== ON PAGE LOAD HANDLER */
 document.addEventListener('DOMContentLoaded', function(event) {
@@ -29,12 +37,26 @@ function beforeExitActions() {
 	Moduware.v0.API.Module.SendCommand(Moduware.Arguments.uuid, 'StopSensor', []);
 }
 
+function calculateDensityAltitude(barometricPressure, outsideTemperature) {
+  return 44330.7692 * (1 - Math.pow((barometricPressure/PSL) / (outsideTemperature/TSL), 0.235));
+  //return (TSL/TLR) * (1 - Math.pow((barometricPressure/PSL) / (outsideTemperature/TSL), (TLR*IGC) / (g*MDA - TLR*IGC)));
+}
+
+function calculateAltimeter(pressure, temperature, humidity) {
+  return ((IGC * temperature) / (g * (MDA * (humidity / 100) + MW * (1 - humidity)))) * Math.log(PSL / pressure);
+}
+
 /** ================ Handlers == */
 function nativeDataUpdateHandler(data) {
 	// getting data and formatting it to numbers with two digits after separator
-	var	humidity = parseFloat(data.humidity).toFixed(3);
-	var temperature = parseFloat(data.temperature).toFixed(2);
-	var	pressure = parseFloat(data.pressure).toFixed(2);
+  var fHumidity = parseFloat(data.humidity);
+	var	humidity = fHumidity.toFixed(3);
+  var fTemperature = parseFloat(data.temperature);
+	var temperature = fTemperature.toFixed(2);
+  var fPressure = parseFloat(data.pressure);
+	var	pressure = fPressure.toFixed(2);
+  var altitude = calculateDensityAltitude(fPressure, fTemperature + 273.15);
+  var altimeter = calculateAltimeter(fPressure, fTemperature + 273.15, fHumidity);
 
 	// current moment
 	var	current_time = new Date().getTime();
@@ -52,16 +74,18 @@ function nativeDataUpdateHandler(data) {
 	lastTimeRecieved = current_time;
 
 	// if data didn't changed we don't need to bother UI
-	if(humidity != lastData.humidity) {
-		lastData.humidity = humidity;
+  // if(humidity != lastData.humidity) {
+	// 	lastData.humidity = humidity;
 		document.getElementById('humidityValue').textContent = humidity;
-	}
-	if(temperature != lastData.temperature) {
-		lastData.temperature = temperature;
+  // }
+  // if(temperature != lastData.temperature) {
+	// 	lastData.temperature = temperature;
 		document.getElementById('temperatureValue').textContent = temperature;
-	}
-  if(pressure!= lastData.pressure) {
-		lastData.pressure = pressure;
+  // }
+  // if(pressure!= lastData.pressure) {
+	// 	lastData.pressure = pressure;
 		document.getElementById('pressureValue').textContent = pressure;
-	}
+  // }
+  document.getElementById('altitudeValue').textContent = altitude.toFixed(2);
+  document.getElementById('altimeterValue').textContent = altimeter.toFixed(2);
 }
